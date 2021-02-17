6294640b3eee24dacdf21baa1cfd250fa16e5442
shravan-Quovantis <shravan.kumar@quovantis.com>
2021-02-17 17:46:58 +0530

    APIs added

--- src/main/java/com/example/SpringJPAMapping/controller/MyController.java
+++ src/main/java/com/example/SpringJPAMapping/controller/MyController.java
@@ -2,6 +2,7 @@ package com.example.SpringJPAMapping.controller;
 
 import com.example.SpringJPAMapping.dao.AddressDao;
 import com.example.SpringJPAMapping.dao.CustomerDao;
+import com.example.SpringJPAMapping.entity.Address;
 import com.example.SpringJPAMapping.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -35,5 +36,40 @@ public class MyController {
     @PutMapping("/customers")
     private Customer update(@RequestBody Customer customer) {
         return customerDao.save(customer);
+    }
+
+    @PostMapping("customers/{id}/addresses")
+    private Optional<Customer> createAddress(@RequestBody List<Address> newAddresses, @PathVariable int id){
+        final Optional<Customer> customerById = customerDao.findById(id);
+        customerById.ifPresent(customer -> {
+            List<Address> existingAddresses = customer.getAddresses();
+            existingAddresses.addAll(newAddresses);
+            customer.setAddresses(existingAddresses);
+            customerDao.save(customer);
+        });
+        return customerDao.findById(id);
+    }
+
+    @GetMapping("customers/{customerId}/addresses/{addressId}")
+    private Optional<Address> getAddress(@PathVariable Integer customerId,
+                                         @PathVariable Integer addressId){
+        return addressDao.findById(addressId);
+    }
+
+    @PutMapping("customers/{customerId}/addresses/{addressId}")
+    private Address updateAddress(@PathVariable Integer customerId,
+                                            @PathVariable Integer addressId,
+                                            @RequestBody Address address){
+        return addressDao.save(address);
+    }
+
+    @DeleteMapping("customers/{customerId}")
+    private void deleteCustomer(@PathVariable Integer customerId){
+        customerDao.deleteById(customerId);
+    }
+
+    @DeleteMapping("customers/{customerId}/addresses/{addressId}")
+    private void deleteAddress(@PathVariable Integer addressId){
+        addressDao.deleteById(addressId);
     }
 }
